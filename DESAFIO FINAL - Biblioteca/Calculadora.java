230b1819e09fc51aa8c99ca15d47a7f1a0683908
Paula Alves <90431867+milkshakedeamora@users.noreply.github.com>
2023-12-06 22:03:52 -0300

    começando a criar

---
+++ DESAFIO FINAL - Biblioteca/Calculadora.java
@@ -0,0 +1,30 @@
+public class Calculadora{
+    public float(int i) somar{
+
+    }
+    public float(int i) multiplicar{
+
+    }
+    public float(int i) subtrair{
+
+    }
+    public float(int i) dividir{
+
+    }
+
+    public Arraylist<float> receberValores(int i){
+        ArrayList<float> numeros = new ArrayList<float>();
+        System.out.printf("[");
+        for(int j = 0; j < i i++){
+            numeros.add();
+
+        }
+        
+        return numeros;
+
+    }
+    
+
+    
+
+}
