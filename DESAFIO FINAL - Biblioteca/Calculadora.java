c82c9a58150927d51877e3afa39cb6e4fa9193e3
Paula Alves <paulacandidoalves@gmail.com>
2023-12-07 22:37:05 -0300

    Melhorando
    MELHORIAS A SEREM FEITAS:
    -Adicionar calcArea, calcPerimetro, jurosSm e jurosCm
    - Adicionar receber String como parametro

--- DESAFIO FINAL - Biblioteca/Calculadora.java
+++ DESAFIO FINAL - Biblioteca/Calculadora.java
@@ -1,29 +1,55 @@
 public class Calculadora{
-    public float(int i) somar{
-
-    }
-    public float(int i) multiplicar{
-
-    }
-    public float(int i) subtrair{
-
-    }
-    public float(int i) dividir{
-
-    }
-
-    public Arraylist<float> receberValores(int i){
-        ArrayList<float> numeros = new ArrayList<float>();
-        System.out.printf("[");
-        for(int j = 0; j < i i++){
-            numeros.add();
-
+    //SOMA
+    public static float somar(double... numeros) {
+        float i = 0;
+        for (double numero : numeros) {
+            i+=numero;
         }
-        
-        return numeros;
-
+        return i;
     }
-    
+
+    //SUBTRAÇÃO
+    public static float subtrair(double... numeros) {
+        float i = (float)numeros[0];
+        for (int j = 1; j < numeros.length; j++) {
+            i-=(float)numeros[j];
+        }
+        return i;
+    }
+
+    //MULTIPLICAÇÃO
+    public static float multiplicar(double... numeros) {
+        float i = 1;
+        for (double numero : numeros) {
+            i*=numero;
+        }
+        return i;
+    }
+    public static float multiplicar(float... numeros) {
+        float i = 1;
+        for (float numero : numeros) {
+            i*=numero;
+        }
+        return i;
+    }
+     
+
+    //DIVISÃO
+    public static float dividir(double... numeros) {
+        float i = (float)numeros[0];
+        for (int j = 1; j < numeros.length; j++) {
+            i/=(float)numeros[j];
+        }
+        return i;
+    }
+    public static void main(String[] args) {
+        System.out.println(somar(3.5,6,2));
+        System.out.println(subtrair(3.5,6,2));
+        System.out.println(multiplicar(3.5,6,2));
+        System.out.println(dividir(3.5,6,2));
+        
+    }
+
 
     
 
